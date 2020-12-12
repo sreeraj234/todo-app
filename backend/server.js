e3662063e9498ec9b3ef09d8c8a0f9a19a5e555b
Sreeraj Parakkat <sreeraj234@gmail.com>
2020-12-13 00:59:55 +0530

    suicidal commit

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,14 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const todos = require("./routes/todos");
 
-require("dotenv").config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MongoURI;
+const uri =
+  "mongodb+srv://sreeraj234:Admin@cluster0.uoy2k.mongodb.net/todo?retryWrites=true&w=majority";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
